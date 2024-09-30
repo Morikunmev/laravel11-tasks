87a5e989ed2bb19b5ccd2002bb8956c90fa09114
Richard <ricky201325@gmail.com>
2024-09-29 21:36:23 -0300

    se añadieron los controladores del dashboard y las plantillas del componente livewire

--- app/Livewire/TaskComponent.php
+++ app/Livewire/TaskComponent.php
@@ -1,23 +1,13 @@
 <?php
 
-namespace App\Http\Livewire;
+namespace App\Livewire;
 
 use Livewire\Component;
-use App\Models\Task;
 
 class TaskComponent extends Component
 {
-    public $tasks;
-
-    public function mount()
-    {
-        // Cargamos todas las tareas desde el modelo Task.
-        $this->tasks = Task::all();
-    }
-
     public function render()
     {
-        // Retornamos la vista 'livewire.task-component' con las tareas.
-        return view('livewire.task-component', ['tasks' => $this->tasks]);
+        return view('livewire.task-component');
     }
 }
